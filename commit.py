791742e0db341180ffe14e07dd9dfa1fd3cb5128
Ingo (School Account) <117789688+Inglan2@users.noreply.github.com>
2024-02-15 03:26:07 +0000

    fix

--- commit.py
+++ commit.py
@@ -2,7 +2,7 @@ import os
 import time
 
 while True:
-    for i range(1, 420):
+    for i in range(1, 420):
         os.system('git commit -m "commit" --allow-empty')
     os.system('git push')
     # time.sleep(1)
